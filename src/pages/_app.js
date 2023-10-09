50a1a77573a25cf8124a332d06c32cfcd0d930b3
siamalrafi <rahathasandipto@gmail.com>
2023-10-09 23:12:29 +0600

    [DONE] setup

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+// pages/_app.js
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+   return (
+      <NextUIProvider>
+         <Component {...pageProps} />
+      </NextUIProvider>
+   );
+}
+
+export default MyApp;
